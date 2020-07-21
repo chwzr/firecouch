ef1d67293ba58413d3fb783f37aed5d80ccac4d5
Felix Koppe <fkoppe@castor.lan>
2020-07-21 14:33:14 +0200

    fix tests

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const firecouch =  require('./lib')
-let db = firecouch('http://', 'localhost:5984', 'admin', 'clubm4te')
+let db = firecouch('http://', 'localhost:5984', 'admin', 'test')
 
 db.collection('users').doc('test4').get().then(d=>{
   console.log(d.snap)
 })
 
-db.collection('users').where('name', '==', "heinz").where('mail', "==", "heinz@koppe.design").get().then(d=>{
+db.collection('users').where('name', '==', "heinz").where('mail', "==", "heinz@ketchup.design").get().then(d=>{
   console.log(d.snap)
 })
 
